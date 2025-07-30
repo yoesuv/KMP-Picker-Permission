fc6ee46c4a24303227ab2bc5162fc3994c6958db
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-07-30 13:28:34 +0700

    Removes unused composable preview
    Removes the unused composable preview from the main activity.
    
    Comments out the experimental lint version in gradle properties.

--- composeApp/src/androidMain/kotlin/com/yoesuv/kmp_pickerpermission/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/yoesuv/kmp_pickerpermission/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.dialogs.init
 
@@ -20,10 +18,4 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
