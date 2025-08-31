6032f22718a61bcb400e4b086c6ec1235f2cfacb
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-08-31 12:48:20 +0700

    Improves audio recording management
    Adds functionality to automatically delete existing .wav files from the cache directory when the RecordViewModel is created and destroyed.
    This ensures a clean state and prevents accumulation of unnecessary audio files.
    
    Also configures the recorder to use PCM_16BIT audio encoding.

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-## KMP Picker Permission
+## KMP Picker Permission ##
+
+:construction: Work in Progress
 
 #### Screenshots Android ####
-| ![](https://i.imgur.com/ncKxlB7.jpeg) | - | ![](https://i.imgur.com/DH1jlh1.jpeg) | - |
+| ![](https://i.imgur.com/ncKxlB7.jpeg) | ![](https://i.imgur.com/9ni63Nl.jpeg) | ![](https://i.imgur.com/DH1jlh1.jpeg) | - |
 | --- | --- | --- | --- |
-| - | ![](https://i.imgur.com/71y4f2r.jpeg) | - | - |
+| - | ![](https://i.imgur.com/71y4f2r.jpeg) | ![](https://i.imgur.com/7BLyVSN.jpeg) | ![](https://i.imgur.com/74FdznK.jpeg) |
 | ![](https://i.imgur.com/njKKmXQ.jpeg) | ![](https://i.imgur.com/1Ukue2j.jpeg) | ![](https://i.imgur.com/eN62CVg.jpeg) | ![](https://i.imgur.com/3zY55LE.jpeg) |
-| - | - | - | - |
+| ![](https://i.imgur.com/BAjJwtJ.jpeg) | ![](https://i.imgur.com/rVbEIbI.jpeg) | ![](https://i.imgur.com/AzX4vu0.jpeg) | - |
 
 #### Screenshots iOS ####
-| - | ![](https://i.imgur.com/c6FHYOL.png) | ![](https://i.imgur.com/P9CG3QP.png) | - |
+| ![](https://i.imgur.com/bxkntrf.png) | ![](https://i.imgur.com/c6FHYOL.png) | ![](https://i.imgur.com/P9CG3QP.png) | - |
 | --- | --- | --- | --- |
 | - | ![](https://i.imgur.com/UuGxC9R.png) | ![](https://i.imgur.com/Y4ks6cp.png) | ![](https://i.imgur.com/n8zJ5G4.png) |
 | - | - | ![](https://i.imgur.com/Sveh54c.png) | ![](https://i.imgur.com/NQF7INU.png) |
--- composeApp/src/commonMain/kotlin/com/yoesuv/kmp_pickerpermission/feature/record/RecordViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/kmp_pickerpermission/feature/record/RecordViewModel.kt
@@ -10,9 +10,18 @@ import dev.icerock.moko.permissions.PermissionsController
 import dev.icerock.moko.permissions.RequestCanceledException
 import dev.icerock.moko.permissions.microphone.RECORD_AUDIO
 import dev.theolm.record.Record
+import dev.theolm.record.config.AudioEncoder
 import dev.theolm.record.config.OutputFormat
 import dev.theolm.record.config.RecordConfig
 import eu.iamkonstantin.kotlin.gadulka.GadulkaPlayer
+import io.github.vinceglb.filekit.FileKit
+import io.github.vinceglb.filekit.cacheDir
+import io.github.vinceglb.filekit.delete
+import io.github.vinceglb.filekit.exists
+import io.github.vinceglb.filekit.isDirectory
+import io.github.vinceglb.filekit.isRegularFile
+import io.github.vinceglb.filekit.list
+import io.github.vinceglb.filekit.name
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -38,11 +47,37 @@ class RecordViewModel(
         Record.setConfig(
             RecordConfig(
                 outputFormat = OutputFormat.WAV,
+                audioEncoder = AudioEncoder.PCM_16BIT
             )
         )
         viewModelScope.launch {
             _permissionState.value =
                 permissionsController.getPermissionState(Permission.RECORD_AUDIO)
+        }
+        // Delete existing .wav files when ViewModel is created
+        deleteWavFilesFromCache()
+    }
+
+    /**
+     * Delete all .wav files from the cache directory
+     */
+    private fun deleteWavFilesFromCache() {
+        viewModelScope.launch {
+            try {
+                val cacheDir = FileKit.cacheDir
+                if (cacheDir.exists() && cacheDir.isDirectory()) {
+                    val files = cacheDir.list()
+                    files.forEach { file ->
+                        if (file.isRegularFile() && file.name.endsWith(".wav", ignoreCase = true)) {
+                            file.delete()
+                            println("Deleted WAV file: ${file.name}")
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                println("Error deleting WAV files from cache: ${e.message}")
+                _error.value = "Failed to clean cache: ${e.message}"
+            }
         }
     }
 
@@ -122,5 +157,13 @@ class RecordViewModel(
     fun stopAudio(player: GadulkaPlayer) {
         player.stop()
         player.release()
+    }
+
+    /**
+     * Delete .wav files when ViewModel is destroyed
+     */
+    override fun onCleared() {
+        super.onCleared()
+        deleteWavFilesFromCache()
     }
 }
